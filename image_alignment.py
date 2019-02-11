bc37d315ceafa0baf7bc04689cd147df8e97dce4
John Van Atta <john.vanatta@motionloft.com>
2019-02-10 19:43:03 -0800

    Corner finding function  working, using masking in saturation channel and cv.findContours().

--- image_alignment.py
+++ image_alignment.py
@@ -2,6 +2,7 @@
 # Python 3
 import random
 import cv2 as cv
+import numpy as np
 
 
 def random_rotation_matrix(max_amount_deg, center_point):
@@ -13,6 +14,27 @@ def random_scale(original_size, max_amount):
 
     return int(round(original_size[0] * scale_amount)), int(round(original_size[1] * scale_amount))
 
+def find_corners(image):
+    # Adapted from Kinght's answer here:
+    # https://stackoverflow.com/questions/8667818/opencv-c-obj-c-detecting-a-sheet-of-paper-square-detection
+    hue_channel, sat_channel, val_channel = cv.split(cv.cvtColor(image, cv.COLOR_BGR2HSV))
+    _, threshed = cv.threshold(sat_channel, 80, 255, cv.THRESH_BINARY)
+    dilate_erode_amount = 50
+    threshed = cv.dilate(threshed, cv.getStructuringElement(cv.MORPH_RECT, (dilate_erode_amount, dilate_erode_amount)))
+    threshed = cv.erode(threshed, cv.getStructuringElement(cv.MORPH_RECT, (dilate_erode_amount, dilate_erode_amount)))
+
+    found_contours = sorted(cv.findContours(threshed, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[-2], key=cv.contourArea)
+    largest_contour = found_contours[-1]
+
+    arclen = cv.arcLength(largest_contour, True)
+    approx = cv.approxPolyDP(largest_contour, 0.02 * arclen, True)
+
+    # Convert output to a list of tuple coordinates
+    return_list = []
+    for corner in approx:
+        return_list.append((corner[0][0], corner[0][1]))
+
+    return return_list
 
 if __name__ == '__main__':
     source_filename = "pcb.jpeg"
@@ -37,8 +59,19 @@ if __name__ == '__main__':
     modified_image = cv.warpAffine(padded_image, random_rotation_matrix(max_rotation_deg, padded_center), (columns, rows),
                                   borderMode=cv.BORDER_REPLICATE)
 
+    # A bit unfortunate to have rotation and warping done separately, since rotations are a subset of affine warps.
+    # Homogeneous coordinates everywhere would make the matrix math cleaner.
+
+    # wait a second, maybe I can just do a matrix multiply?
+
+    #print (find_corners(modified_image).shape)
+    corners = find_corners(modified_image)
     while True:
         k = cv.waitKey(1)
         if k == 'q' or k == 27 or k == 'Q' or k == 1048603 or k == 1048689:
             break
+
+        for corner in corners:
+            cv.circle(modified_image, corner, 10, (100, 0, 40), 3)
         cv.imshow("modified", modified_image)
+
