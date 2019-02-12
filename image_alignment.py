3f87b9bf85a43974f6573d0075b43214961eea6b
John Van Atta <john.vanatta@motionloft.com>
2019-02-11 23:12:00 -0800

    Further refactoring. Added and reworked comments.

--- image_alignment.py
+++ image_alignment.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # Python 3
 import random
-import sys
 
 import cv2 as cv
 import numpy as np
 
+''' There's some weirdness here if this goes above 75 or so, wherein the hue shifts appear visually discontinuous
+'''
 def random_hue_shift(image, max_amount):
     hue_channel, sat_channel, val_channel = cv.split(cv.cvtColor(image, cv.COLOR_BGR2HSV))
-    # There's some weirdness here if this goes above 75 or so, wherein the hue shifts appear visually
-    # discontinuous.
     shift_amount = random.randrange(0, int(max_amount))
     adjustment = np.ones(hue_channel.shape, dtype=np.uint8) * shift_amount
 
     return cv.cvtColor(cv.merge([hue_channel + adjustment, sat_channel, val_channel]), cv.COLOR_HSV2BGR)
+
 
 def random_scale(image, max_amount):
     scale_amount = 1.0 + random.uniform(-1 * max_amount, max_amount)
@@ -27,6 +27,9 @@ def random_rotation(image, output_size, max_amount_deg, center_point):
     return cv.warpAffine(image, rotation_matrix, output_size, borderMode=cv.BORDER_REPLICATE)
 
 
+''' max_projective values must be very small to avoid transforming the image far outside its boundaries. There's no
+    compensating translation to adjust for that.
+'''
 def random_distort(image, max_affine, max_projective):
     distortion_array = np.array([
         [1 + random.uniform(-1 * max_affine, max_affine), random.uniform(-1 * max_affine, max_affine), 0],
@@ -37,15 +40,17 @@ def random_distort(image, max_affine, max_projective):
     return cv.warpPerspective(image, distortion_array, (image.shape[0], image.shape[1]), borderMode=cv.BORDER_REPLICATE, flags=cv.WARP_INVERSE_MAP)
 
 
+''' Adapted from Kinght's answer here:
+    https://stackoverflow.com/questions/8667818/opencv-c-obj-c-detecting-a-sheet-of-paper-square-detection
+    Masking in saturation works well for the two base PCB images I tested on, but it's certainly not infallible.
+    Consistency of the lighting, background, and PCB color is important. Masking on value also works.
+'''
 def find_corners(image):
-    # Adapted from Kinght's answer here:
-    # https://stackoverflow.com/questions/8667818/opencv-c-obj-c-detecting-a-sheet-of-paper-square-detection
-    # Masking in saturation works well for the two base PCB images I tested on, but it's certainly not infallible.
-    # Consistency of the lighting, background, and PCB color is important. Masking on value also works.
     dilate_erode_amount = 80
     channel_threshold = 80
     hue_channel, sat_channel, val_channel = cv.split(cv.cvtColor(image, cv.COLOR_BGR2HSV))
     _, threshed = cv.threshold(sat_channel, channel_threshold, 255, cv.THRESH_BINARY)
+    # A dilate/erode pass helps remove the holes in the mask from the components on the PCB
     threshed = cv.dilate(threshed, cv.getStructuringElement(cv.MORPH_RECT, (dilate_erode_amount, dilate_erode_amount)))
     threshed = cv.erode(threshed, cv.getStructuringElement(cv.MORPH_RECT, (dilate_erode_amount, dilate_erode_amount)))
 
@@ -61,10 +66,13 @@ def find_corners(image):
     return approx
 
 
+''' Sort coordinates found from cv.findContours() into a known order: TL, TR, BR, BL by Euclidean distance.
+    The coordinates closest to (0,0) will be selected as the TL point, etc. 
+    This isn't completely robust, extreme enough perspective transforms could result in the same point
+    getting chosen twice.
+'''
 def sort_corners(unsorted_corners, image_height, image_width):
-    # This isn't completely robust, extreme enough perspective transforms could result in the same point
-    # getting chosen twice
-    # Order: TL, TR, BR, BL
+
     def distance_cost(a_tuple, b_tuple):
         return (a_tuple[0] - b_tuple[0])**2 + (a_tuple[1] - b_tuple[1])**2
 
@@ -82,8 +90,9 @@ def sort_corners(unsorted_corners, image_height, image_width):
 
     return sorted_corners
 
+
 if __name__ == '__main__':
-    # These random values normally generate good output, but there's a possibility a 'perfect storm' alignment
+    # These random values normally generate good output, but there's a possibility a 'perfect storm'
     # will result in the image being transformed beyond the edge of padding.
     # If so, the corner recovery will fail.
     source_filename = "pcb.jpeg"
@@ -106,17 +115,18 @@ if __name__ == '__main__':
                                      (adjusted_image.shape[1] // 2, adjusted_image.shape[0] // 2))
     adjusted_image = random_distort(adjusted_image, max_affine_warp, max_projective_warp)
 
-    corners = sort_corners(find_corners(adjusted_image), adjusted_image.shape[1], adjusted_image.shape[0])
 
     output_x_size = 600
     output_y_size = 450
-
     output_corners = np.array([
         [0, 0],
         [output_x_size - 1, 0],
         [output_x_size - 1, output_y_size - 1],
         [0, output_y_size - 1]], dtype="float32")
 
+    corners = sort_corners(find_corners(adjusted_image), adjusted_image.shape[1], adjusted_image.shape[0])
+
+    # Map the detected PCB corner coordinates to the output image corners
     correction_matrix = cv.getPerspectiveTransform(corners, output_corners)
     correct_image = cv.warpPerspective(adjusted_image, correction_matrix, (600, 450))
 
