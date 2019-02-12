29a6042dff720ca05e06b6c136d0d0f3a7ece353
John Van Atta <john.vanatta@motionloft.com>
2019-02-12 00:30:46 -0800

    Change rectify_variations to work on bulk input. Unfortunately, a bug has cropped up that makes the corner finding often fail. Committing and pushing to get github's dif tools.

--- rectify_variations.py
+++ rectify_variations.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # Python 3
+import glob
+import os
+import sys
+
 import cv2 as cv
 import numpy as np
 
@@ -8,9 +12,7 @@ import numpy as np
     Masking in saturation works well for the two base PCB images I tested on, but it's certainly not infallible.
     Consistency of the lighting, background, and PCB color is important. Masking on value also works.
 '''
-def find_corners(image):
-    dilate_erode_amount = 80
-    channel_threshold = 80
+def find_corners(image, channel_threshold, dilate_erode_amount):
     hue_channel, sat_channel, val_channel = cv.split(cv.cvtColor(image, cv.COLOR_BGR2HSV))
     _, threshed = cv.threshold(sat_channel, channel_threshold, 255, cv.THRESH_BINARY)
     # A dilate/erode pass helps remove the holes in the mask from the components on the PCB
@@ -54,37 +56,64 @@ def sort_corners(unsorted_corners, image_height, image_width):
     return sorted_corners
 
 
+''' Detect the corners of a directory of images of colorful planar quadrangles. For example, pictures of rectangular PCBs.
+    Warp them to a consistent view. The detected corners of the quadrangles will become the true corners of the new,
+    rectangular image.
+    
+    A clear separation from the background for the majority of the quadrangles is necessary. Detection is done in saturation,
+    it could be easily modified to detect based on value or hue.
+'''
 if __name__ == '__main__':
     # These random values normally generate good output, but there's a possibility a 'perfect storm'
     # will result in the image being transformed beyond the edge of padding.
     # If so, the corner recovery will fail.
-    source_filename = "variation.jpg"
-
-    input_image = cv.imread(source_filename)
-
+    source_files = "variations"
     output_x_size = 600
     output_y_size = 450
-    output_corners = np.array([
-        [0, 0],
-        [output_x_size - 1, 0],
-        [output_x_size - 1, output_y_size - 1],
-        [0, output_y_size - 1]], dtype="float32")
+    detection_mask_threshold = 80
+    dilate_erode_amount = 80
 
-    corners = sort_corners(find_corners(input_image), input_image.shape[1], input_image.shape[0])
+    images_to_process = []
+    if os.path.isdir(source_files):
+        for filename in glob.glob("{0}/*.jpg".format(source_files)):
+            images_to_process.append(filename)
+    else:
+        images_to_process.append(source_files)
 
-    # Map the detected PCB corner coordinates to the output image corners
-    correction_matrix = cv.getPerspectiveTransform(corners, output_corners)
-    corrected_image = cv.warpPerspective(input_image, correction_matrix, (600, 450))
+    print("Starting work on {0} image files...".format(len(images_to_process)))
 
-    while True:
-        k = cv.waitKey(1)
-        if k == 'q' or k == 27 or k == 'Q' or k == 1048603 or k == 1048689:
-            break
+    for image_filename in images_to_process:
+        input_image = cv.imread(image_filename)
+        if input_image is None:
+            sys.exit("Bad input, check your filename: {0}".format(input_image))
 
-        for corner in corners:
-            cv.circle(input_image, (corner[0], corner[1]), 10, (100, 0, 40), 3)
-        cv.imshow("modified", input_image)
-        cv.imshow("corrected", corrected_image)
+        # Detect the corners in the input image, then sort them to the same TL, TR, BR, BL order as the output
+        detected_corners = sort_corners(find_corners(input_image, detection_mask_threshold, dilate_erode_amount), input_image.shape[1], input_image.shape[0])
+        print("\n \n")
+        print(input_image.shape[1], input_image.shape[0])
+        print(detected_corners)
+        output_corners = np.array([
+            [0, 0],
+            [output_x_size - 1, 0],
+            [output_x_size - 1, output_y_size - 1],
+            [0, output_y_size - 1]], dtype="float32")
+
+        # Map the detected PCB corner coordinates to the output image corners
+        correction_matrix = cv.getPerspectiveTransform(detected_corners, output_corners)
+        corrected_image = cv.warpPerspective(input_image, correction_matrix, (600, 450))
+
+        while True:
+            k = cv.waitKey(1)
+            # Press escape or q for next image
+            if k == 'q' or k == 27 or k == 'Q' or k == 1048603 or k == 1048689:
+                break
+
+            for corner in detected_corners:
+                cv.circle(input_image, (corner[0], corner[1]), 10, (100, 0, 40), 3)
+            cv.imshow("modified", input_image)
+            cv.imshow("corrected", corrected_image)
+
+    print("All done!")
 
 
 
